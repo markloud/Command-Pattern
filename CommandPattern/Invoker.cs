a22739af8085dce5758b042d73324f7b164058db
Mark Anthony Chua <MarkAnthony.Chua@infor.com>
2016-04-23 22:42:04 +0800

    separate class files

---
+++ CommandPattern/Invoker.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CommandPattern
+{
+    /// <summary>
+    /// The 'Invoker' class
+    /// </summary>
+    class Invoker
+    {
+        // Initializers
+        private Receiver _calculator = new Receiver();
+        private List<ICommand> _commands = new List<ICommand>();
+        private int _current = 0;
+
+        public void Redo(int levels)
+        {
+            Console.WriteLine("\n---- Redo {0} levels ", levels);
+            // Perform redo operations
+            for (int i = 0; i < levels; i++)
+            {
+                if (_current < _commands.Count - 1)
+                {
+                    ICommand command = _commands[_current++];
+                    command.Execute();
+                }
+            }
+        }
+
+        public void Undo(int levels)
+        {
+            Console.WriteLine("\n---- Undo {0} levels ", levels);
+            // Perform undo operations
+            for (int i = 0; i < levels; i++)
+            {
+                if (_current > 0)
+                {
+                    ICommand command = _commands[--_current] as ICommand;
+                    command.UnExecute();
+                }
+            }
+        }
+
+        public void Compute(char @operator, int operand)
+        {
+            // Create command operation and execute it
+            ICommand command = new CalculatorCommand(
+              _calculator, @operator, operand);
+            command.Execute();
+
+            // Add command to undo list
+            _commands.Add(command);
+            _current++;
+        }
+    }
+}
